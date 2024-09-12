47f6c14f99ed6c725a415ad83d4a1c16f5b1d411
Halflove <29326375+HalfloveJAR@users.noreply.github.com>
2024-09-12 14:18:50 -0700

    :tada: Add more projects to all projects list

--- src/components/AllProjects.vue
+++ src/components/AllProjects.vue
@@ -35,6 +35,37 @@
             picture: 'src/assets/snowdamage.jpg',
             description:
               'A Minecraft Spigot plugin that allows you to modify the damage snowballs and eggs deal.'
+          },
+          {
+            name: 'Loot Llama',
+            link: 'https://github.com/HalfloveJAR/LootLlama',
+            picture: 'src/assets/lootllama.jpg',
+            description: 'A Minecraft Spigot plugin that spawns a llama pinata for drop parties.'
+          },
+          {
+            name: 'PZ Spawnpoint Generator',
+            link: 'https://github.com/HalfloveJAR/pz-spawnpoint-gen',
+            picture: 'src/assets/pzspawn.png',
+            description:
+              'Input coordinates and get spawn config options for Project Zomboid servers.'
+          },
+          {
+            name: 'Briefcase',
+            link: 'https://github.com/HalfloveJAR/Briefcase',
+            description: 'A Minecraft Spigot plugin that stores item in a custom inventory.'
+          },
+          {
+            name: 'Ore Restore',
+            link: 'https://github.com/HalfloveJAR/Ore-Restore',
+            picture: 'src/assets/ore.png',
+            description:
+              'A Minecraft Spigot plugin that regenerates specified blocks over time like an RPG.'
+          },
+          {
+            name: 'Love Quests',
+            link: 'https://github.com/HalfloveJAR/Love-Quests',
+            description:
+              'A detailed Minecraft Spigot plugin that gives tasks for players to complete for rewards.'
           }
         ]
       }
